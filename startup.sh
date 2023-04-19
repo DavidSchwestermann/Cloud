7a52210187cf4e8d5949afbd5375cb055aae8995
DavidSchwestermann <91324050+DavidSchwestermann@users.noreply.github.com>
2023-04-19 20:07:46 +0200

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker run -d -p 9090:9090 -v /home/david/prometheus.yml:/etc/prometheus/prometheus.yml -v prom:/etc/prometheus/ --name prometheus-yannnnnnic prom/prometheus
+sudo docker run -d -p 3000:3000 --name grafana -v grafana:/etc/grafana/ grafana/grafana
